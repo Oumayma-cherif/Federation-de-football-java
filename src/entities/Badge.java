4471f37326b42157f0ea3efc4978c71f1c9d7ca5
Ahmed Missaoui <ahmed.missaoui1@esprit.tn>
2022-04-20 22:34:53 +0100

    my first commit

---
+++ src/entities/Badge.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author ksaay
+ */
+public class Badge {
+    private int id,nb;
+    private String nomB,logoB;
+
+    public Badge() {
+      
+    }
+    @Override
+    public String toString() {
+        //return "Badge{" + "nb=" + nb + ", nomB=" + nomB + ", logoB=" + logoB + '}';
+         return "Badge{" + "nomB=" + nomB + ", nb=" + nb + ", logoB=" + logoB + '}';
+    }
+
+   public Badge(Badge x)
+   {
+      
+      
+
+          this.id = x.id;
+       this.logoB = x.logoB;
+       this.nb = x.nb;
+       this.nomB = x.nomB;
+      
+     
+       
+   }
+
+    public Badge(int id,String nomB, String logoB,int nb) {
+        this.id = id;
+        this.nomB = nomB;
+        this.nb = nb;
+        this.logoB = logoB;
+    }
+    public Badge(String nomB, String logoB,int nb) {
+       
+        this.nomB = nomB;
+        this.nb = nb;
+        this.logoB = logoB;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNb() {
+        return nb;
+    }
+
+    public void setNb(int nb) {
+        this.nb = nb;
+    }
+
+    public String getNomB() {
+        return nomB;
+    }
+
+    public void setNomB(String nomB) {
+        this.nomB = nomB;
+    }
+
+    public String getLogoB() {
+        return logoB;
+    }
+
+    public void setLogoB(String logoB) {
+        this.logoB = logoB;
+    }
+    
+    
+    
+}
